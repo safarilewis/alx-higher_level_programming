003e2d48afc1bb2885bfdeb7a69cf9c039b7c8a3
Lewis Safari <lleewwii1@gmail.com>
2022-08-29 03:20:25 -0400

    Task 1, retrieving an element from an array

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,8 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    for i in my_list:
-        if idx == i:
-            print("Element at {:d} is {:d}".format(i, my_list[i]))
-        elif idx > i:
-            return None
+    else:
+        for i in my_list:
+            if i == idx:
+                return my_list[idx]
