36b8c67c3f86c97e046e17154b08b96a7511bc06
Lewis Safari <lleewwii1@gmail.com>
2022-08-29 03:13:46 -0400

    Task 1, retrieving an element from an array

---
+++ 0x03-python-data_structures/tests/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    else:
+        for i in my_list:
+            if i == idx:
+                return my_list[idx]
+        
