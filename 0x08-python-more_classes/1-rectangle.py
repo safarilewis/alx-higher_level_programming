929e495564c98e3dc857a553ff1bb002596c6642
Lewis Safari <lleewwii1@gmail.com>
2022-09-19 16:57:59 -0400

    Task 2

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,3 +12,29 @@ class Rectangle:
         self.width = width
 
         @property
+        def width(self):
+            """Getter for the private instance attibute width"""
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+            """Setter for the private instance attribute width"""
+            if type(value) is not int:
+                raise TypeError("width must be an integer")
+            if value < 0:
+                raise ValueError("width must be >=0")
+            self.__width = value
+
+        @property
+        def height(self):
+            """Getter for te private instance attribute width"""
+            return self.__height
+
+        @height.setter
+        def height(self,value):
+            """Setter for the height value"""
+            if type(value) is not int:
+                raise TypeError("height must be an integer")
+            if height < 0:
+                raise ValueError("height must be >=0")
+            self.__height = value
