ca4b3350b2ad44ced7e1ac1147e85320858053be
Lewis Safari <lleewwii1@gmail.com>
2022-09-19 17:37:46 -0400

    Delete rectangle

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -11,10 +11,6 @@ class Rectangle:
         self.width = width
         self.height = height
 
-    def __del__(self):
-        """returns a string when a rectangle is deleted"""
-        return("Bye rectangle...")
-
     @property
     def width(self):
         """getter for the private instance attribute width"""
@@ -64,3 +60,7 @@ class Rectangle:
     def __repr__(self):
         """returns a string representation of the rectangle for reproduction"""
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+    
+    def __del__(self):
+        """returns a string when a rectangle is deleted"""
+        print("Bye rectangle...")
