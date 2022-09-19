9bc74535d1ca7b9e4b9a21d8c78c08ff1bfe5174
Lewis Safari <lleewwii1@gmail.com>
2022-09-19 16:17:04 -0400

    Stuff

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pthon3
+"""
+Defines an empty Rectangle class
+"""
+
+
+class Rectangle:
+    """Empty representation of a rectangle"""
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,14 @@
+"""
+Defines a class rectangle
+"""
+
+
+class Rectangle:
+    """Representation of a rectangle"""
+    def __init__(self, width=0,height=0):
+        """Initialized the rectangle"""
+        self.height = height
+        self.width = width
+
+        @property
+        
