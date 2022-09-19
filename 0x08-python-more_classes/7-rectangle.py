75f7d494848a3c69a68f058bcb5dda3d43f08b0d
Lewis Safari <lleewwii1@gmail.com>
2022-09-19 17:56:05 -0400

    Changing representation of #

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -8,13 +8,18 @@ class Rectangle:
     """Representation of a rectangle"""
 
     number_of_instances = 0
-    print_symbol = '#'
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """Initializes the rectangle"""
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
+
+    def __del__(self):
+        """prints a string when an instance has been deleted"""
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
 
     @property
     def width(self):
@@ -58,15 +63,10 @@ class Rectangle:
         """returns printable string representation of the rectangle"""
         string = ""
         if self.__width != 0 and self.__height != 0:
-            string += "\n".join(self.print_symbol * self.__width
+            string += "\n".join(str(self.print_symbol) * self.__width
                                 for j in range(self.__height))
         return string
 
     def __repr__(self):
         """returns a string representation of the rectangle for reproduction"""
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
-
-    def __del__(self):
-        """returns a string when a rectangle is deleted"""
-        print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
