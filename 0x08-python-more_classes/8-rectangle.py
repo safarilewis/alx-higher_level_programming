02ddc5291ff3d64a6cbdc57db72dcd4d4b0ab51b
Lewis Safari <lleewwii1@gmail.com>
2022-09-19 18:17:56 -0400

    Adding a static method

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -12,12 +12,12 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        """Returns the biggest rectangle based on the area"""
+        """returns the biggest rectangle based on the area"""
         if type(rect_1) is not Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if type(rect_2) is not Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area >= rect_2.area:
+        if rect_1.area() >= rect_2.area():
             return rect_1
         return rect_2
 
