ed1ae02d3b9f3de244f7e592910d2d3622f5ae37
Lewis Safari <lleewwii1@gmail.com>
2022-09-19 17:45:14 -0400

    Removing whitespaces

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -56,11 +56,11 @@ class Rectangle:
             string += "\n".join("#" * self.__width
                                 for j in range(self.__height))
         return string
-    
+
     def __repr__(self):
         """returns a string representation of the rectangle for reproduction"""
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
-    
+
     def __del__(self):
         """returns a string when a rectangle is deleted"""
         print("Bye rectangle...")
