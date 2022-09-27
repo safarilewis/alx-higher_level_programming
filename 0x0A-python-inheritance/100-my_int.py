411255a93d0fe8ced4560a0dc62435f8ecc526d6
Lewis Safari <lleewwii1@gmail.com>
2022-09-26 23:05:34 -0400

    Inheritance

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Subclass of int"""
+
+
+class MyInt(int):
+    
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """Is obj a subclass of a_class"""
-    return (issubclass(type(obj), a_class) and type(obj)!= a_class)
+    return (issubclass(type(obj), a_class) and type(obj) != a_class)
