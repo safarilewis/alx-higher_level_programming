240cc79d26d79421087c01fa97dbc5c0a2c44629
Lewis Safari <lleewwii1@gmail.com>
2022-09-26 22:02:46 -0400

    Inheritance

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """An empty class"""
 class BaseGeometry():
-    
+    """A class containing an unimplimented funct"""
     def area(self):
         RaiseException("area() is not implemented")
