0a4c8d42b731ae71c7f07788e740cb15ffe7b04f
Lewis Safari <lleewwii1@gmail.com>
2022-09-26 22:50:07 -0400

    Inheritance

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -21,4 +21,4 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         """Formats to string"""
-        return ("[Rectangle]{:d}/{:d}".format(self.__width, self.__height))
+        return ("[Rectangle] {:d}/{:d}".format(self.__width, self.__height))
