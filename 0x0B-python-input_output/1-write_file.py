4918d892e24e1127823665c67cf397c55e5cd93c
Lewis Safari <lleewwii1@gmail.com>
2022-09-28 15:39:15 -0400

    Input and Output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,4 +8,4 @@ Opening a file and writing to it
 def write_file(filename="", text=""):
     """Reading the file using with"""
     with open(filename, 'w',encoding = "utf-8") as f:
-        print(f.read(),end='')
+        f.write(text)
