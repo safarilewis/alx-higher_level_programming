5789b21a6efdaba2c1c83797dfb888fa260d58a4
Lewis Safari <lleewwii1@gmail.com>
2022-09-28 23:36:02 -0400

    Input and Output

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,6 +3,7 @@
 Appends a string at the end of a text file
 """
 
+
 def append_write(filename="", text=""):
     """Adds a string to the end of a text file"""
     with open(filename, 'a', encodeing='utf-8') as f:
