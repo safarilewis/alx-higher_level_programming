aa9b938d7a314444cc8c33d8e91634211976b7fb
Lewis Safari <lleewwii1@gmail.com>
2022-09-28 23:48:01 -0400

    Input and Output

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import json
-
     """
     returns JSON representation of an object(string)
     """
 
+import json
+
 
     def to_json_string(my_obj):
         """returns the json representation of my_obj"""
-        json.dumps(my_obj)
+        return json.dumps(my_obj)
