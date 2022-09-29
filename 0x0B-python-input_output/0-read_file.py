468bffa0f6c13e87536981370df47a31f82fbaeb
Lewis Safari <lleewwii1@gmail.com>
2022-09-28 23:18:57 -0400

    Input and Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,5 @@ Opening a file and reading it's contents
 
 def read_file(filename=""):
     """Reading the file using with"""
-    with open(filename, 'r',encoding = "utf-8") as f:
-        print(f.read(),end='')
+    with open(filename, 'r', encoding="utf-8") as f:
+        print(f.read(), end='')
