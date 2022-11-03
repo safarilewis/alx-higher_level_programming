37dbf3c7995207d8cc589edf6a55324aa6c3cdd7
Lewis Safari <lleewwii1@gmail.com>
2022-11-03 14:53:32 +0300

    Python almost a circle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Base Class"""
+
+class Base:
+    """Class """
+    __nb_objects=0
+    
+    def __init__(self,id=None):
+
+    if (id is not None):
+        self.id = id
+    else:
+        __nb_objects +=
