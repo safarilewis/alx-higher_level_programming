4dbe7b2dc8f15d3043aff6c78cb27162af377447
Lewis Safari <lleewwii1@gmail.com>
2022-11-03 15:11:08 +0300

    Python almost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,8 +8,8 @@ class Base:
 
     def __init__(self, id=None):
         """Constructor"""
-    if (id is not None):
-        self.id = id
-    else:
-        Base.__nb_objects +=1
-        self.id = Base.__nb_objects
+        if (id is not None):
+            self.id = id
+        else:
+            Base.__nb_objects +=1
+            self.id = Base.__nb_objects
