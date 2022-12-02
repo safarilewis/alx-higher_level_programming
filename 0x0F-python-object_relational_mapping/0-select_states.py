79e5e013a3d912004549d9e8192bbf795a66fd36
Lewis Safari <lleewwii1@gmail.com>
2022-12-02 17:12:02 +0300

    debugging is poison

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,7 +3,7 @@ import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(host="localhost", port=3306,user=sys.argv[1],password=sys.argv[2],db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306,user=sys.argv[1],passwd=sys.argv[2],db=sys.argv[3])
     cursor = db.cursor();
     cursor.execute("SELECT * FROM states ORDER BY states.id;")
     data = cursor.fetchall()
