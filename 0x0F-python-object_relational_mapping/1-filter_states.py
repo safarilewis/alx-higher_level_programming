8ebec66183d2a1d2d4f549b68706f77fc4cddb83
Lewis Safari <lleewwii1@gmail.com>
2022-12-02 17:28:10 +0300

    still unable to fix error

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-# This script Lists all states starting with N.
-import MySQLdb
+#Prints all states starting with a capital N
 import sys
+import MySQLdb
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id;")
-    result = cur.fetchall()
-    for row in result:
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id;")
+    data = cursor.fechall()
+    for row in data:
         if row[1].startswith("N"):
-            print(row)
-    cur.close()
+            print (row)
+    cursor.close()
     db.close()
+    
