9d15b22000a673b3ee74857eb6c4c05b6e3eab85
Lewis Safari <lleewwii1@gmail.com>
2022-12-02 17:27:26 +0300

    bugs make me sad

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-#Prints all states starting with a capital N
-import sys
+# This script Lists all states starting with N.
 import MySQLdb
+import sys
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    curs = db.cursor()
-    curs.execute("SELECT * FROM states ORDER BY states.id;")
-    data = curs.fechall()
-    for row in data:
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id;")
+    result = cur.fetchall()
+    for row in result:
         if row[1].startswith("N"):
-            print (row)
-    curs.close()
+            print(row)
+    cur.close()
     db.close()
