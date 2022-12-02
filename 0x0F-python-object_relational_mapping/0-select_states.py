026babc7e675edbe3cf15d8c8b4fd3193d8c9962
Lewis Safari <lleewwii1@gmail.com>
2022-12-02 17:08:29 +0300

    fixing cursor method

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306,user=sys.argv[1],password=sys.argv[2],db=sys.argv[3])
-    cursor = MySQLdb.cursor();
+    cursor = db.cursor();
     cursor.execute("SELECT * FROM states ORDER BY states.id")
     data = cursor.fetchall()
     for row in data:
