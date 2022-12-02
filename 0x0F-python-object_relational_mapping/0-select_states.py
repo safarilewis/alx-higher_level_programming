ec7d81ca07e3d883581864596bc19a998c47ef0b
Lewis Safari <lleewwii1@gmail.com>
2022-12-02 17:09:26 +0300

    adding semicolon to wuery

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,7 @@ import sys
 if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306,user=sys.argv[1],password=sys.argv[2],db=sys.argv[3])
     cursor = db.cursor();
-    cursor.execute("SELECT * FROM states ORDER BY states.id")
+    cursor.execute("SELECT * FROM states ORDER BY states.id;")
     data = cursor.fetchall()
     for row in data:
         print(row)
