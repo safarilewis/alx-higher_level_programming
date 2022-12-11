f313de477b65716c1e5223afb0b1db701ccd7c2b
Lewis Safari <lleewwii1@gmail.com>
2022-12-11 17:02:17 +0300

    fixing typo

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY states.id;")
-    data = cursor.fechall()
+    data = cursor.fetchall()
     for row in data:
         if row[1].startswith("N"):
             print (row)
