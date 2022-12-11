9a90cc2c648e030b033e3eecf809fc35c0332b47
Lewis Safari <lleewwii1@gmail.com>
2022-12-11 23:17:50 +0300

    upload

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,8 +7,9 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("""SELECT * FROM cities WHERE state LIKE BINARY '%s'
-                    ORDER BY cities.id""", [sys.argv[4]])
+    cursor.execute("""SELECT cities.id, cities.name, '%s' FROM cities
+                    JOIN states ON cities.state_id = states.id
+                    ORDER BY cities.id;""", [sys.argv[4]])
     data = cursor.fetchall()
     for row in data:
         print(row)
