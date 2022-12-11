ea809e00ffe8c994357b8347c12749b4cf45cd96
Lewis Safari <lleewwii1@gmail.com>
2022-12-11 17:04:22 +0300

    fixing pycodestyle

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,13 +4,13 @@ import sys
 import MySQLdb
 if __name__ == "__main__":
     """Connects to database and gets data"""
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY states.id;")
     data = cursor.fetchall()
     for row in data:
         if row[1].startswith("N"):
-            print (row)
+            print(row)
     cursor.close()
     db.close()
-    
