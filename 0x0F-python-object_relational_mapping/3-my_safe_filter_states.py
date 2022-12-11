a7b586b4781888104339ed5c37386546830ace2c
Lewis Safari <lleewwii1@gmail.com>
2022-12-11 22:40:18 +0300

    upload

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Displays all values in the states table where name matches the argument"""
+import sys
+import MySQLdb
+if __name__ == '__main__':
+    """Connects to database and gets data"""
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    query = """SELECT * 
+                FROM states WHERE name LIKE BINARY '%s' ORDER BY id;"""
+    cursor.execute(query,[sys.argv[4]])
+    data = cursor.fetchall()
+    for row in data:
+        print(row)
+    cursor.close()
+    db.close()
