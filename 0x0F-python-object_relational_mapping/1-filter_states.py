e9fb7a0f0d88a0ab441713b65bb5dfb22d94d69e
Lewis Safari <lleewwii1@gmail.com>
2022-12-11 17:15:04 +0300

    task 2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     """Connects to database and gets data"""
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
-    cursor = db.cursor()
+    cursor = db.cursor()    
     cursor.execute("SELECT * FROM states ORDER BY states.id;")
     data = cursor.fetchall()
     for row in data:
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
 """Displays all values in the states table where name matches the argument"""
+import sys
+import MySQLdb
+if __name__ == '__main__':
+    """Connects to database and gets data"""
+    db = MySQLdb.connect(host="localhost",port=3306,user=sys.argv[1],
+                        passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM STATES WHERE name IS {} ORDER BY states.id".format(sys.argv[4]))
+    data = cursor.fetchall()
+    for state in data:
+        print(state)
+    cursor.close()
+    db.close()
