71405b5db21287f17fa8296da53dd6ba0c5beedb
Lewis Safari <lleewwii1@gmail.com>
2022-12-11 17:00:36 +0300

    upload

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-#Prints all states starting with a capital N
+"""Prints all states starting with a capital N"""
 import sys
 import MySQLdb
 if __name__ == "__main__":
+    """Connects to database and gets data"""
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY states.id;")
---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+"""Displays all values in the states table where name matches the argument"""
