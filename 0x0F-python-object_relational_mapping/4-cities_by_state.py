ecf19c472e7a7cad87a768848eb397854d06b01d
Lewis Safari <lleewwii1@gmail.com>
2022-12-11 22:54:25 +0300

    upload

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Displays all values in the states table where name matches the argument"""
+import sys
+import MySQLdb
+if __name__ == '__main__':
+    """Connects to database and gets data"""
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("""SELECT * FROM cities WHERE state LIKE BINARY '%s'
+                    ORDER BY cities.id""", [sys.argv[4]])
+    data = cursor.fetchall()
+    for row in data:
+        print(row)
+    cursor.close()
+    db.close()
