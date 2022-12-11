13f08c1a013a7bd09bbf8cac23a8a90e6323eba0
Lewis Safari <lleewwii1@gmail.com>
2022-12-11 17:30:05 +0300

    alive

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,8 +4,8 @@ import sys
 import MySQLdb
 if __name__ == '__main__':
     """Connects to database and gets data"""
-    db = MySQLdb.connect(host="localhost",port=3306,user=sys.argv[1],
-                        passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id;".format(sys.argv[4]))
     data = cursor.fetchall()
