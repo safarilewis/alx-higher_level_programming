c62039dbd10ee1d7c464b7443a83f320970bdb52
safarilewis <lleewwii1@gmail.com>
2022-12-15 17:19:51 +0300

    task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a POST request with data passed as parameters
-curl -sdL "email=test@gmail.com&subject=I&will&always&be&here&for&PLD" "$1"
+curl -sLd "email=test@gmail.com&subject=I&will&always&be&here&for&PLD" "$1"
