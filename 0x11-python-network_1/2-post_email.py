17c91ccc6919a1eac9dede511aec4f7ed18ff4bf
safarilewis <lleewwii1@gmail.com>
2022-12-17 12:02:28 +0300

    decoding response

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -13,4 +13,4 @@ if __name__ == "__main__":
     req = urllib.request.Request(url,data)
     with urllib.request.urlopen(req) as response:
         response = response.read()
-    print(response)
+    print(response.decode('utf-8'))
