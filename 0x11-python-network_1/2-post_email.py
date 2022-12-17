2375b0b0069f849318964db77b4ad2e6e59c29c5
safarilewis <lleewwii1@gmail.com>
2022-12-17 11:59:35 +0300

    changes

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,8 +7,8 @@ import urllib.parse
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = sys.argv[2]
-    #data = urllib.parse.urlencode(value)
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value)
     data = value.encode('ascii')
     req = urllib.request.Request(url,data)
     with urllib.request.urlopen(req) as response:
