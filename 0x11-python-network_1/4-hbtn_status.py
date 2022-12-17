65d51354a94aca592566e2500ce1df0395d66215
safarilewis <lleewwii1@gmail.com>
2022-12-17 12:36:20 +0300

    update

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,4 +6,4 @@ import requests
 if __name__ == "__main__":
     response = requests.get("https://alx-intranet.hbtn.io/status")
     print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.content))
+    print("\t- content: {}".format(response.text))
