e71d2439abd6c6d03696238094e56e75387d0472
safarilewis <lleewwii1@gmail.com>
2022-12-18 22:46:54 +0300

    update

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     response = requests.get("https://api.github.com/repos/{}/{}/commits".format(sys.argv[1], sys.argv[2]))
     x = 0
     while x < 10:
-        print("{}: {}".format(response.json().get("sha"), response.json().get("committer.name")))
+        print("{}: {}".format(response.get("sha"), response.get("commit").get("author").get("name")))
         x += 1
