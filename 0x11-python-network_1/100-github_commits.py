5cb5cc36001f0b7f7784c1917147927107b2e596
safarilewis <lleewwii1@gmail.com>
2022-12-18 22:52:54 +0300

    updating

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,6 +7,6 @@ import requests
 if __name__ == "__main__":
     response = requests.get("https://api.github.com/repos/{}/{}/commits".format(sys.argv[1],
                                                     sys.argv[2]), params = {'per_page': 10})
-    for commits in response:
+    for commits in response.json():
         print("{}: {}".format(commits.get("sha"), commits.get("commit").get("author").get("name")))
    
