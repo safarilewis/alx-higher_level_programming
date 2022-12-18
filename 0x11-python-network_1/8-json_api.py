57db551da6b4bdf240cffe4bdf1a0c61df0c2478
safarilewis <lleewwii1@gmail.com>
2022-12-18 22:10:25 +0300

    style checking

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,18 +5,18 @@ import requests
 
 
 if __name__ == "__main__":
-    value = {'q':""}
+    value = {'q': ""}
     if len(sys.argv) > 1:
         value["q"] = sys.argv[1]
-    response = requests.post("http://0.0.0.0:5000/search_user",data=value)
+    response = requests.post("http://0.0.0.0:5000/search_user", data=value)
     try:
-        empty=["",{},None]
+        empty = ["", {}, None]
         if "id" not in response.json().keys() or "name" \
             not in response.json().keys():
             print("No result")
         else:
             print("[{}] {}".format(response.json().get("id"),
-                    response.json().get("name")))
+                                   response.json().get("name")))
     except ValueError:
         print("Not a valid JSON")
 
