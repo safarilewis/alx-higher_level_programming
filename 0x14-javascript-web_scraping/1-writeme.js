60e58a5122259df828addcb01da9d8b5d4d9d440
safarilewis <lleewwii1@gmail.com>
2023-01-17 14:05:31 +0300

    changing from sync

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const fs = require("fs");
 const text = process.argv[3];
-try {
-  fs.writeFile(process.argv[2], text, "utf-8");
-} catch (err) {
-  console.error(err);
-}
+fs.writeFile(process.argv[2], text, "utf-8", (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
